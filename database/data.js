8e6d2f2ed03d834261bce6896b6ce58452bb60f5
withsjb <withsjb1@gmail.com>
2023-06-08 17:36:46 +0900

    datainsert

---
+++ database/data.js
@@ -0,0 +1,18 @@
+const questions = [
+  {
+    id: 1,
+    text: 11111111111,
+    question: "진빈의 코드",
+    options: ["Object-Oriented", "Object-Based", "Procedural", "Procedural"],
+  },
+];
+
+const photo = "4a892037-f315-495a-af0d-2f611a7ea3ef_.JPG";
+
+const answers = [0];
+
+module.exports = {
+  questions,
+  photo,
+  answers,
+};
