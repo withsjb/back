054aff08b04e5ef149bcf9e1010b9e1dd1a88d46
withsjb <withsjb1@gmail.com>
2023-06-13 06:23:03 +0900

    errormi

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -30,7 +30,7 @@ const {
   getphoto,
 } = require("../Controllers/AuthControllers");
 const { checkUser } = require("../Middlewares/AuthMiddlewares");
-const uploadMiddleware = require("../middlewares/MulterMiddleware");
+const uploadMiddleware = require("../Middlewares/MulterMiddleware");
 
 //자동생성 되는거보니 기능인듯
 
